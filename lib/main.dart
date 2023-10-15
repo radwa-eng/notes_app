a79a531c82b3b8c25e9c3d52b11f03b257e2f456
radwa nagy <radwanagy19@gmail.com>
2023-10-15 07:32:47 +0300

    update modelButtomsheet

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:noteapp/constants.dart';
-import 'package:noteapp/cubits/add_note_cubit/cubit/add_note_cubit.dart';
 import 'package:noteapp/models/note_model.dart';
 import 'package:noteapp/simple_bloc_observ.dart';
 import 'package:noteapp/views/notes_view.dart';
